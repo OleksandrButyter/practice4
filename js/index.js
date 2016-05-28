f7d58349cf74ff35c36c1c629242fe57124fbdaa
Oleksandr <olekbut@gmail.com>
2016-05-28 10:56:03 +0000

    Add JS

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
 // JavaScript File
-console.log('Hello World!')
+
+
+function getAge(age) {
+if (age < 18) { 
+    return 'Young'
+} else {
+    return 'Adult'
+}    
+}
+
+var myAge = Number(prompt('Enter your name'));
+alert(getAge(myAge));
